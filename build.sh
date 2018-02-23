615d384a2440fe0a0f626939ff719fcba82ebc42
dboissin <damien.boissin@web-education.net>
2018-02-23 17:57:57 +0100

    Improve build

---
+++ build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ -z ${USER_UID:+x} ]
+then
+  export USER_UID=1000
+  export GROUP_GID=1000
+fi
+
+clean () {
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle clean
+}
+
+buildGradle () {
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle shadowJar install publishToMavenLocal
+}
+
+publish () {
+  if [ -e "?/.gradle" ] && [ ! -e "?/.gradle/gradle.properties" ]
+  then
+    echo "odeUsername=$NEXUS_ODE_USERNAME" > "?/.gradle/gradle.properties"
+    echo "odePassword=$NEXUS_ODE_PASSWORD" >> "?/.gradle/gradle.properties"
+    echo "sonatypeUsername=$NEXUS_SONATYPE_USERNAME" >> "?/.gradle/gradle.properties"
+    echo "sonatypePassword=$NEXUS_SONATYPE_PASSWORD" >> "?/.gradle/gradle.properties"
+  fi
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle publish
+}
+
+for param in "$@"
+do
+  case $param in
+    clean)
+      clean
+      ;;
+    buildGradle)
+      buildGradle
+      ;;
+    install)
+      buildGradle
+      ;;
+    publish)
+      publish
+      ;;
+    *)
+      echo "Invalid argument : $param"
+  esac
+  if [ ! $? -eq 0 ]; then
+    exit 1
+  fi
+done
+
